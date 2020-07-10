6ca2d35efc1b6a0b4c15ed398ebfcad3dba0e7ed
Sayan Goswami <goswami.sayan47@gmail.com>
2020-07-10 17:50:55 +0530

    Decrease font size for basic section.

--- templates/basic.py
+++ templates/basic.py
@@ -3,17 +3,13 @@ from string import Template
 LAYOUT = Template(r"""
 \begin{table}
 \setlength\tabcolsep{0pt}
+\small
 \begin{tabularx}{\linewidth}{X r}
 	\multirow{4}{*}{\fontsize{36}{44}\selectfont{{\bf $firstname} $lastname}} & \href{$website}{$website} \\ & \href{mailto:$email}{$email} \\ & \href{skype:$skype}{$skype}\\ & \href{tel:$phone}{$phone}
 \end{tabularx}
 
 \begin{center}
-\small
 (Interests: $interests)
 \end{center}
-
 \end{table}
-
-
-
 """)
