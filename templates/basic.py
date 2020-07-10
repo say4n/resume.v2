d20362ee83a6732093ed88029e39ccbb4d03b2b0
Sayan Goswami <goswami.sayan47@gmail.com>
2020-07-10 17:45:48 +0530

    Move interests inside table.

--- templates/basic.py
+++ templates/basic.py
@@ -6,11 +6,14 @@ LAYOUT = Template(r"""
 \begin{tabularx}{\linewidth}{X r}
 	\multirow{4}{*}{\fontsize{36}{44}\selectfont{{\bf $firstname} $lastname}} & \href{$website}{$website} \\ & \href{mailto:$email}{$email} \\ & \href{skype:$skype}{$skype}\\ & \href{tel:$phone}{$phone}
 \end{tabularx}
-\end{table}
 
 \begin{center}
 \small
 (Interests: $interests)
 \end{center}
+
+\end{table}
+
+
 
 """)
