60e92d4a00feb0b60802319f3909279e5f92778c
Sayan Goswami <goswami.sayan47@gmail.com>
2020-07-10 17:35:40 +0530

    Add basic section.

---
+++ templates/basic.py
@@ -0,0 +1,16 @@
+from string import Template
+
+LAYOUT = Template(r"""
+\begin{table}
+\setlength\tabcolsep{0pt}
+\begin{tabularx}{\linewidth}{X r}
+	\multirow{4}{*}{\fontsize{36}{44}\selectfont{{\bf $firstname} $lastname}} & \href{$website}{$website} \\ & \href{mailto:$email}{$email} \\ & \href{skype:$skype}{$skype}\\ & \href{tel:$phone}{$phone}
+\end{tabularx}
+\end{table}
+
+\begin{center}
+\small
+(Interests: $interests)
+\end{center}
+
+""")
