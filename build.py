d3d292ace382676c3cc0178687cfd3ad4a4246c5
Sayan Goswami <goswami.sayan47@gmail.com>
2020-07-11 22:47:44 +0530

    Add coursework.

--- build.py
+++ build.py
@@ -1,22 +1,21 @@
 #! /usr/bin/env python3
 
 import os
-from yaml import Loader, load
-from templates import basic, document, education, heading, publications
 
+from yaml import Loader, load
+
+from templates import (basic, courses, document, education, heading,
+                       publications)
 
 DATA_DIR = "data"
 LAYOUR_DIR = "layouts"
 BUILD_DIR = "build"
 
 
-with open(os.path.join(DATA_DIR, "achievements.yml"), "rt") as f:
-    achievements_data = load(f, Loader=Loader)
-
 with open(os.path.join(DATA_DIR, "basic.yml"), "rt") as f:
     basic_data = load(f, Loader=Loader)
 
-    basic_string = basic.LAYOUT.substitute(firstname=basic_data["name"]["first"],
+    basic_string = basic.LAYOUT.safe_substitute(firstname=basic_data["name"]["first"],
                                             lastname=basic_data["name"]["last"],
                                             website=basic_data["website"],
                                             email=basic_data["email"],
@@ -26,19 +25,41 @@ with open(os.path.join(DATA_DIR, "basic.yml"), "rt") as f:
 
 with open(os.path.join(DATA_DIR, "courses.yml"), "rt") as f:
     courses_data = load(f, Loader=Loader)
+    section = heading.HEADING.safe_substitute(heading="Relevant Coursework")
+    moocs = courses_data["MOOCs"]
+    undergrad = courses_data["Undergraduate Coursework"]
+
+    undergrad_listing = ""
+    for u in undergrad:
+        if u["relevant"]:
+            undergrad_listing += courses.UNDERGRAD_LISTING.safe_substitute(name=u["name"])
+
+    undergrad_string = courses.UNDERGRAD_LAYOUT.safe_substitute(listing=undergrad_listing)
+
+    mooc_listing = ""
+    for m in moocs:
+        mooc_listing += courses.MOOC_LISTING.safe_substitute(name=m["name"],
+                                                        where=m["where"])
+
+    mooc_string = courses.MOOC_LAYOUT.safe_substitute(listing=mooc_listing)
+
+    courses_string = courses.LAYOUT.safe_substitute(heading=section,
+                                                    undergrad=undergrad_string,
+                                                    moocs=mooc_string)
+
 
 with open(os.path.join(DATA_DIR, "education.yml"), "rt") as f:
     education_data = load(f, Loader=Loader)
-    section = heading.HEADING.substitute(heading="Education")
+    section = heading.HEADING.safe_substitute(heading="Education")
     listing = ""
 
     for e in education_data:
-        listing += education.LISTING.substitute(school=e["name"],
+        listing += education.LISTING.safe_substitute(school=e["name"],
                                                 location=e["location"],
                                                 duration=e["duration"],
                                                 description=r"\\ ".join(e["description"]))
 
-    education_string = education.LAYOUT.substitute(heading=section,
+    education_string = education.LAYOUT.safe_substitute(heading=section,
                                                     listing=listing)
 
 with open(os.path.join(DATA_DIR, "experience.yml"), "rt") as f:
@@ -50,20 +71,23 @@ with open(os.path.join(DATA_DIR, "positions.yml"), "rt") as f:
 with open(os.path.join(DATA_DIR, "projects.yml"), "rt") as f:
     projects_data = load(f, Loader=Loader)
 
+with open(os.path.join(DATA_DIR, "achievements.yml"), "rt") as f:
+    achievements_data = load(f, Loader=Loader)
+
 with open(os.path.join(DATA_DIR, "publications.yml"), "rt") as f:
     publications_data = load(f, Loader=Loader)
-    section = heading.HEADING.substitute(heading="Publications")
+    section = heading.HEADING.safe_substitute(heading="Publications")
     listing = ""
 
     for p in publications_data:
         if p["published"] == True:
-            listing += publications.LISTING.substitute(where=p["where"],
+            listing += publications.LISTING.safe_substitute(where=p["where"],
                                                         when=p["when"],
                                                         url=p["url"],
                                                         title=p["title"],
                                                         authors=p["authors"])
 
-    publications_string = publications.LAYOUT.substitute(heading=section,
+    publications_string = publications.LAYOUT.safe_substitute(heading=section,
                                                             listing=listing)
 
 with open(os.path.join(DATA_DIR, "references.yml"), "rt") as f:
@@ -79,9 +103,11 @@ def generate_document():
 
     {education_string}
 
+    {courses_string}
+
     {publications_string}
     """
-    return document.LAYOUT.substitute(content=content)
+    return document.LAYOUT.safe_substitute(content=content)
 
 
 if __name__ == "__main__":
---
+++ templates/courses.py
@@ -0,0 +1,37 @@
+from string import Template
+
+MOOC_LISTING = Template(r"""
+\item $name ($where) $\cdot$
+""")
+
+MOOC_LAYOUT = Template(r"""
+\textbf{MOOCs: }
+\begin{itemize*}
+\itemsep0em
+
+$listing
+
+\end{itemize*}\\
+""")
+
+UNDERGRAD_LISTING = Template(r"""
+\item $name $\cdot$
+""")
+
+UNDERGRAD_LAYOUT = Template(r"""
+\textbf{Undergraduate Coursework: }
+\begin{itemize*}
+\itemsep0em
+
+$listing
+
+\end{itemize*}\\
+""")
+
+LAYOUT = Template(r"""
+$heading\\
+
+$undergrad
+
+$moocs
+""")
